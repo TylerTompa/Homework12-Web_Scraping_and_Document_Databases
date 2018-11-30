d7b15fb8b1afcaec43488f2d3451eeec539f7ed3
Tyler Tompa <tylertompa@email.com>
2018-11-30 14:26:07 -0500

    create setup for Flask website

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,87 +1,144 @@
+####################################################################################################
+"""
+Tyler Tompa
+2018, November, 30
+UNCC Data Analytics Boot Camp Homework 12- Web Scraping and Document Databases
+"""
+####################################################################################################
+
+import os
+
 from bs4 import BeautifulSoup as bs
 import requests
-# from splinter import Browser
-# import splinter
 
-# import pandas as pd
+import pandas as pd
 
-# nasa_news_site = requests.get("https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest")
-# nasa_news_site
+from flask import Flask, render_template, jsonify
 
-# nasa_news_site_soup = bs(nasa_news_site.text, "html.parser")
+def scrape():
 
+    nasa_mars_file_path = os.path.join("NewsNASAMarsExplorationProgram", "News_NASA_Mars_Exploration_Program.html")
+    nasa_mars_html = open(nasa_mars_file_path, "r").read()
 
-# # In[4]:
+    nasa_news_site_soup = bs(nasa_mars_html, "html.parser")
 
+    nasa_news_article_titles_html = nasa_news_site_soup.find_all("div", class_="content_title")
+    nasa_news_article_paragraphs_html = nasa_news_site_soup.find_all("div", class_="article_teaser_body")
 
-# nasa_news_article_titles_html = nasa_news_site_soup.find_all("div", class_="content_title")
-# nasa_news_article_paragraphs_html = nasa_news_site_soup.find_all("div", class_="'article_teaser_body")
+    article_titles_list = []
+    nasa_news_article_paragraphs_list = []
 
+    nasa_news_article_title = nasa_news_article_titles_html[0].find("a").text
+    nasa_news_article_title
 
-# # In[5]:
+    nasa_news_article_paragraph = nasa_news_article_paragraphs_html[0].text.replace("\n", "")
+    nasa_news_article_paragraph
 
+    nasa_news_article_dictionary = {"title": nasa_news_article_title,
+                                "paragraph": nasa_news_article_paragraph}
+    nasa_news_article_dictionary
 
-# nasa_news_article_paragraphs_html
+    featured_mage_base_url = "https://www.jpl.nasa.gov"
+    featured_image_starting_site_extension = "/spaceimages/?search=&category=Mars"
 
+    site_with_featured_image_url = featured_mage_base_url + featured_image_starting_site_extension
+    featured_image_site = requests.get(site_with_featured_image_url)
+    featured_image_soup = bs(featured_image_site.text, "html.parser")
 
-# # In[6]:
+    featured_image_url_extension = featured_image_soup.find("a", class_=["button", "fancybox"]).get("data-fancybox-href")
+    featured_image_url_extension
 
+    featured_image_url = featured_mage_base_url + featured_image_url_extension
+    featured_image_url
 
-# nasa_news_article_titles_html
+    featured_image_url_dictionary = {"featured_image_url": featured_image_url}
+    featured_image_url_dictionary
 
+    mars_twitter_page = requests.get("https://twitter.com/marswxreport?lang=en")
+    mars_twitter_page
 
-# # In[7]:
+    mars_twitter_page_soup = bs(mars_twitter_page.text, "html.parser")
 
+    mars_twitter_page_latest_tweet = mars_twitter_page_soup.find("p", class_="TweetTextSize").text
+    mars_twitter_page_latest_tweet
 
-# nasa_news_article_paragraphs_html
+    mars_twitter_page_latest_tweet_dictionary = {"latest_tweet": mars_twitter_page_latest_tweet}
+    mars_twitter_page_latest_tweet_dictionary
 
+    mars_space_facts_url = "https://space-facts.com/mars/"
 
-# # In[8]:
+    mars_space_facts_tables = pd.read_html(mars_space_facts_url)
 
+    mars_space_facts_tables
 
-# nasa_news_article_titles = []
+    type(mars_space_facts_tables)
 
+    len(mars_space_facts_tables)
 
-# # In[9]:
+    mars_space_facts_tables[0]
 
+    mars_space_facts_table = mars_space_facts_tables[0]
+    mars_space_facts_table
 
-# for nasa_news_article_title in nasa_news_article_titles_html:
-#     try:
-#         nasa_news_article_titles.append(nasa_news_article_title.find("a").text.strip())
-        
-#     except:
-#         print("Error")
+    mars_facts_table_dictionary = {"facts_table" : mars_space_facts_table}
+    mars_facts_table_dictionary
 
+    mars_space_facts_html_table_string = mars_space_facts_tables[0].to_html()
 
-# # In[10]:
+    mars_space_facts_html_table_string
 
+    cerebrus_hemisphere_site = requests.get("https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced")
+    cerebrus_hemisphere_site
 
-# nasa_news_article_titles
+    cerebrus_hemisphere_soup = bs(cerebrus_hemisphere_site.text, "html.parser")
 
-from flask import Flask
+    schiaparelli_hemisphere_site = requests.get("https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced")
+    schiaparelli_hemisphere_site
+
+    schiaparelli_hemisphere_soup = bs(schiaparelli_hemisphere_site.text, "html.parser")
+
+    syrtis_major_hemisphere_site = requests.get("https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced")
+    syrtis_major_hemisphere_site
+
+    syrtis_major_hemisphere_soup = bs(syrtis_major_hemisphere_site.text, "html.parser")
+
+    valles_marineris_hemisphere_site = requests.get("https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced")
+    valles_marineris_hemisphere_site
+
+    valles_marineris_hemisphere_soup = bs(valles_marineris_hemisphere_site.text, "html.parser")
+
+    mars_hemisphere_soups_list = [cerebrus_hemisphere_soup, schiaparelli_hemisphere_soup, syrtis_major_hemisphere_soup, valles_marineris_hemisphere_soup]
+
+    mars_hemisphere_images_list = [mars_hemisphere.find("div", class_="downloads") for mars_hemisphere in mars_hemisphere_soups_list]
+
+    mars_hemisphere_image_urls_list = [mars_hemisphere_image.find("a").get("href") for mars_hemisphere_image in mars_hemisphere_images_list]
+    mars_hemisphere_image_urls_list
+
+    mars_hemisphere_names_list = ["Cerberus", "Schiaparelli", "Syrtis_Major", "Valles_Marineris"] 
+
+    mars_hemisphere_name_and_image_urls_dictionary = dict(zip(mars_hemisphere_names_list, mars_hemisphere_image_urls_list))
+    mars_hemisphere_name_and_image_urls_dictionary
+
+    mars_data_dictionary = {"article": nasa_news_article_dictionary,
+                        "featured_image": featured_image_url_dictionary,
+                        "latest_weather_tweet": mars_twitter_page_latest_tweet_dictionary,
+                        "facts_table": mars_facts_table_dictionary,
+                        "hemisphere_images": mars_hemisphere_name_and_image_urls_dictionary}
+    mars_data_dictionary
+
+    return mars_data_dictionary
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Mission to Mars"
+    return render_template("index.html", text="Mars")
 
 @app.route("/scrape")
-def scrape():
-    nasa_news_site = requests.get("https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest")
-    nasa_news_site_soup = bs(nasa_news_site.text, "html.parser")
-    nasa_news_article_titles_html = nasa_news_site_soup.find_all("div", class_="content_title")
-
-    nasa_news_article_titles = []
-
-    for nasa_news_article_title in nasa_news_article_titles_html:
-        try:
-            nasa_news_article_titles.append(nasa_news_article_title.find("a").text.strip())
-        
-        except:
-            print("Error")
-
-    return nasa_news_article_titles
+def scrape_route():
+    mars_dictionary = scrape()
+    return jsonify(mars_dictionary)
 
 if __name__ == "__main__":
     app.run(debug=True)
