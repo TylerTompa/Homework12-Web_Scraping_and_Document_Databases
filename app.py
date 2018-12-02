a4de5f76c5718a66647c6658e71160ffe3634e23
Tyler Tompa <tylertompa@email.com>
2018-12-01 23:47:57 -0500

    comment and clean up app.py file

--- app.py
+++ app.py
@@ -14,21 +14,13 @@ import sys
 
 app = Flask(__name__)
 
+# Here we use PyMongo to establish a Mongo connection and create a new database named mars_database.
 mongo = PyMongo(app, uri= "mongodb://localhost:27017/mars_database")
 
+# Here we print a statement so that we can look in our console to tell whether our code is working as intended.
 print("####################################################################################################")
 print("define mongo")
 print("####################################################################################################")
-
-# connection = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(connection)
-# db = client.mars_database
-
-# db.mars.drop()
-
-# db.mars.insert_many(
-#     scrape_mars.scrape()
-# )
 
 @app.route("/")
 def index():
@@ -36,24 +28,39 @@ def index():
 
     return render_template("index.html", mars = mars_data)
 
+    # Here we print a statement so that we can look in our console to tell whether our code is working as intended.
+    # In order to print statements on a flask page, we must use the sys library and print to stdout.
     print("####################################################################################################", file=sys.stdout)
     print("Load home page", file=sys.stdout)
     print("####################################################################################################", file=sys.stdout)
 
 @app.route("/scrape")
 def scrape():
+
+    # We import the scrape_mars .py file we already created,
+    # and use the scrape() function we defined therein.
     scraped_mars_data = scrape_mars.scrape()
+
+    # Here we print a statement so that we can look in our console to tell whether our code is working as intended.
+    # In order to print statements on a flask page, we must use the sys library and print to stdout.
     print("####################################################################################################", file=sys.stdout)
     print("Run scrape function", file=sys.stdout)
     print("####################################################################################################", file=sys.stdout)
     
+    # We update our MongoDB database with the data which we scraped in our scrape_mars.py file.
     mongo.db.collection.update({}, scraped_mars_data, upsert=True)
+
+    # Here we print a statement so that we can look in our console to tell whether our code is working as intended.
+    # In order to print statements on a flask page, we must use the sys library and print to stdout.
     print("####################################################################################################", file=sys.stdout)
     print("####################################################################################################", file=sys.stdout)
     print("Update databse", file=sys.stdout)
     print("####################################################################################################", file=sys.stdout)
 
     return redirect("/")
+
+    ## Here we print a statement so that we can look in our console to tell whether our code is working as intended.
+    # In order to print statements on a flask page, we must use the sys library and print to stdout.
     print("####################################################################################################", file=sys.stdout)
     print("Redirect to index", file=sys.stdout)
     print("####################################################################################################", file=sys.stdout)
